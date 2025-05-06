f7177e09d2f551da27299ade7307effe8944250c
DoulaD <Douladoumbia@gmail.com>
2025-05-06 14:14:58 -0400

    room reservation v1

---
+++ HotelOperations/src/test/java/com/hotel/Reservation.java
@@ -0,0 +1,10 @@
+package com.hotel;
+
+public class Reservation {
+    public static void main(String[] args) {
+
+        private String roomType;
+        private int Price;
+        private int numberOfNights;
+    }
+}
---
+++ HotelOperations/src/test/java/com/hotel/Room.java
@@ -0,0 +1,37 @@
+package com.hotel;
+
+public class Room {
+    public Room() {
+    }
+
+
+    private int numberOfBeds;
+    private double price;
+    private boolean occupied;
+    private boolean dirty;
+
+
+    public Room(int numberOfBeds, double price, boolean occupied, boolean available) {
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
+        this.occupied = occupied;
+        this.dirty = available;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    public boolean isAvailable() {
+        return dirty;
+    }
+}
+
