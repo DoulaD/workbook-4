047e43a25e2c08d379e9ffcaa5a7405a53aaa226
DoulaD <Douladoumbia@gmail.com>
2025-05-07 09:04:15 -0400

    main and employee

---
+++ HotelOperations/src/main/java/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Room room1 = new Room(2,160.0,false, true);
+    }
+}
--- HotelOperations/src/test/java/com/hotel/Room.java
+++ HotelOperations/src/test/java/com/hotel/Room.java
@@ -1,8 +1,6 @@
 package com.hotel;
 
 public class Room {
-    public Room() {
-    }
 
 
     private int numberOfBeds;
@@ -31,7 +29,7 @@ public class Room {
     }
 
     public boolean isAvailable() {
-        return dirty;
+        return !dirty && !occupied;
     }
 }
 
