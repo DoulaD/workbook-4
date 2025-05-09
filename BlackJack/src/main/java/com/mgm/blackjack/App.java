fddfbdecb22668e590256235016f73f64fe1b12b
DoulaD <Douladoumbia@gmail.com>
2025-05-08 21:17:49 -0400

    class set up

---
+++ BlackJack/src/main/java/com/mgm/blackjack/App.java
@@ -0,0 +1,11 @@
+package com.mgm.blackjack;
+
+import com.mgm.blackjack.models.Card;
+
+public class App {
+
+   public static void main(String[] args) {
+       Card card = new Card("spades",5,);
+
+  }
+}
